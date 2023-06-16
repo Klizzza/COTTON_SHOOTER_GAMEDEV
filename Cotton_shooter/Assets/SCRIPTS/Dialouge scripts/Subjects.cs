b708f6724c9bb8d026b6f8ec114e68381d8baa26
Bagelcornia <newtonesunvidis@gmail.com>
2023-06-16 16:50:41 -0600

    hbuhybh

---
+++ Cotton_shooter/Assets/SCRIPTS/Dialouge scripts/Subjects.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New subject", menuName = "Create new subject") ]
+public class Subjects : ScriptableObject
+{
+    public Sprite CharacterFace;
+
+}
